9ccf470b5e72587b1702458056940947a228fa50
Mehdi <mehdiasefi@gmail.com>
2016-10-19 22:46:42 -0700

    redundant imports removed.

--- flydelay/src/main/java/me/soulmachine/DelayCount.java
+++ flydelay/src/main/java/me/soulmachine/DelayCount.java
@@ -2,18 +2,12 @@ package me.soulmachine;
 
 import java.io.IOException;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import me.soulmachine.FilterTopK.TopKReducer;
-import me.soulmachine.FilterTopK.TopKMapper;
 
 
 public class DelayCount {
---
+++ flydelay/src/test/java/me/soulmachine/DelayCountTest.java
@@ -0,0 +1,7 @@
+package me.soulmachine;
+
+/**
+ * Created by mehdiasefi on 10/19/16.
+ */
+public class DelayCountTest {
+}
